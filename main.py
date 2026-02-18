e9f91e63237fb5f38486ae2e5b1cc464c6b4cc6c
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-18 12:49:40 +0530

    reated the mainmpy file

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.password_validator import validate_password
+
+password = input("Enter password: ")
+
+if validate_password(password):
+    print("Password is valid")
+else:
+    print("Password is invalid")
