65d3a0ddd8bfa45751fc3e667fda0cae1164d5dd
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-02-18 11:40:07 +0530

    Uppdated the calci py and added more two functions

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,15 @@
 def add(a: int, b: int) -> int:
     return a + b
 
+def subtract(a: int, b: int) -> int:
+    if a < 0:
+        raise ValueError("Negative numbers are not allowed")
+    if b < 0:
+        raise ValueError("Negative numbers are not allowed")
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    return a * b
 
 def divide(a: int, b: int) -> float:
     if b == 0:
